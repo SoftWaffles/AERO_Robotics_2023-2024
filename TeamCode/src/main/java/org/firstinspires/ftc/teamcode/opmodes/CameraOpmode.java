027ed709e8d1b06fd339d25e6e3755488ecc5f2e
Kevin Mathew <thekevinmathew@gmail.com>
2023-12-20 12:03:43 -0600

    Testing VisionPortal
    Created Opmode and processor for new vision portal implementations as well as created use for FTC Dashboard

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/CameraOpmode.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.vision.CustomProcessor;
+import org.firstinspires.ftc.vision.VisionPortal;
+@Autonomous()
+public class CameraOpmode extends OpMode {
+  private CustomProcessor test_proc;
+  private VisionPortal visionPortal;
+
+  @Override
+  public void init() {
+    test_proc = new CustomProcessor();
+    visionPortal = VisionPortal.easyCreateWithDefaults(hardwareMap.get(WebcamName.class, "Webcam 1"), test_proc);
+  }
+
+  @Override
+  public void init_loop() {
+  }
+
+  @Override
+  public void start() {
+    visionPortal.stopStreaming();
+  }
+
+  @Override
+  public void loop() {
+    telemetry.addData("Identified", test_proc.getSelection());
+  }
+}
