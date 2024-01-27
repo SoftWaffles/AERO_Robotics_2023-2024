38e5dc8d61d64bccc17021d2ea865bd270fb62af
Kevin Mathew <thekevinmathew@gmail.com>
2024-01-27 02:13:27 -0600

    removed extra files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ConceptAprilTagEasy_ANDRIOD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/ConceptAprilTagEasy_ANDRIOD.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -61,6 +62,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
+@Disabled
 @TeleOp(name = "APRILTAG_TEST", group = "Concept")
 
 public class ConceptAprilTagEasy_ANDRIOD extends LinearOpMode {
